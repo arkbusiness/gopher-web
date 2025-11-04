727c0dfa7eb9af9b96d89bfa35c9a565a6ba1588
orsdev <oluwadareysamuel@gmail.com>
2025-11-04 06:03:25 +0100

    refactor: replace Geist with Inter font and update project configuration

---
+++ constants/common.ts
@@ -0,0 +1,5 @@
+export const ARK_META = {
+  title: "Gopher | All-in-One Event Platform for Trade Shows & Exhibitions",
+  description:
+    "Gopher is a trade fair application designed to help organizers run smoother physical events, enables exhibitors to sell more, and attendees discover products and exhibitors, book one-on-one meetings, chat live with exhibitors, and makes the purchase process before, during and after the fair easy.",
+};
