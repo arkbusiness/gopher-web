e926b4ed7cca209ecad4c040b1ddd642a8756698
orsdev <oluwadareysamuel@gmail.com>
2025-11-12 17:47:37 +0100

    refactor: wrap Hero section content in Container component for consistent padding

--- features/landing/components/hero/components/hero.tsx
+++ features/landing/components/hero/components/hero.tsx
@@ -1,21 +1,24 @@
 import { HeroContent } from "./hero-content";
 import { HeroSlider } from "./hero-slider";
 import { HERO_CONTENT, HERO_SLIDES } from "../config/hero-data";
+import { Container } from "@/components/layouts/container";
 
 export function Hero() {
   return (
-    <section className="px-6 md:px-21 py-12 md:py-16">
-      <div className="flex lg:flex-row flex-col gap-12 lg:gap-16">
-        {/* Left Side - Content */}
-        <div className="flex-1">
-          <HeroContent content={HERO_CONTENT} />
-        </div>
+    <section className="py-12 md:py-16">
+      <Container>
+        <div className="flex lg:flex-row flex-col gap-12 lg:gap-16">
+          {/* Left Side - Content */}
+          <div className="flex-1">
+            <HeroContent content={HERO_CONTENT} />
+          </div>
 
-        {/* Right Side - Slider */}
-        <div className="relative lg:w-[clamp(240px,50vw,640px)]">
-          <HeroSlider slides={HERO_SLIDES} />
+          {/* Right Side - Slider */}
+          <div className="relative lg:w-[clamp(240px,50vw,640px)]">
+            <HeroSlider slides={HERO_SLIDES} />
+          </div>
         </div>
-      </div>
+      </Container>
     </section>
   );
 }
